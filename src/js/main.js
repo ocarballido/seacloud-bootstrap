33617d8dfe233034a6031faf702145fe7a8488bd
ocarballido <ocarballido@gmail.com>
2023-12-07 15:53:57 +0100

    updating images, adding swiper package, adding new color util

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,33 @@ import '../scss/main.scss';
 // Load Bootstrap init
 import {initBootstrap} from "./bootstrap.js";
 
+// import Swiper JS
+import Swiper from 'swiper';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 // Loading bootstrap with optional features
 initBootstrap({
-  tooltip: false,
-  popover: false,
-  toasts: false,
+	tooltip: false,
+	popover: false,
+	toasts: false,
 });
 
 // Your app code
-console.log(`Hello ${process.env.HELLO}`);
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 1,
+	spaceBetween: 0,
+	// autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    // },
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	modules: [ Autoplay, Navigation, Pagination ],
+});
