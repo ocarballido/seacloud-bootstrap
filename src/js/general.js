4eb6ce299f19824dd5d8ca3d310352f3a54c7224
ocarballido <ocarballido@gmail.com>
2024-09-17 19:33:54 +0200

    appbar and mobile menu offcanvas refactor

--- src/js/general.js
+++ src/js/general.js
@@ -3,6 +3,8 @@ export const appbarScrolled = () => {
     const navElement = document.querySelector('#appbar');
     const appbarLogo = document.querySelector('#appbarLogo');
     const appbarLogoSymbol = document.querySelector('#appbarLogoSymbol');
+    const scrollTop = document.querySelectorAll('.scroll-top');
+    const scrolled = document.querySelectorAll('.scrolled');
 
     window.addEventListener('scroll', () => {
         if (window.scrollY >= 300) {
@@ -11,12 +13,26 @@ export const appbarScrolled = () => {
             navElement.classList.add('py-1');
             appbarLogo.classList.add('d-none');
             appbarLogoSymbol.classList.remove('d-none', 'py-4');
+            // scrollTop.classList.add('d-none');
+            // scrolled.classList.remove('d-none');
+            scrollTop.forEach(function(element) {
+                element.classList.add('d-none');
+            });
+            scrolled.forEach(function(element) {
+                element.classList.remove('d-none');
+            });
         } else if (window.scrollY < 300) {
             navElement.classList.remove('navbar-scrolled');
             navElement.classList.remove('py-1');
             navElement.classList.add('py-4');
             appbarLogo.classList.remove('d-none');
             appbarLogoSymbol.classList.add('d-none');
+            scrollTop.forEach(function(element) {
+                element.classList.remove('d-none');
+            });
+            scrolled.forEach(function(element) {
+                element.classList.add('d-none');
+            });
         }
     });
 }
