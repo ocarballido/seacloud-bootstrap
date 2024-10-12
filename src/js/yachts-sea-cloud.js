5f3af430c1ec37577b1b2b3418ea0f031be8167a
ocarballido <ocarballido@gmail.com>
2024-10-12 21:57:04 +0200

    page finished

--- src/js/yachts-sea-cloud.js
+++ src/js/yachts-sea-cloud.js
@@ -12,12 +12,13 @@ gsap.registerPlugin(ScrollTrigger)
 
 // import Swiper JS
 import Swiper from 'swiper';
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, FreeMode, Navigation, Pagination } from 'swiper/modules';
 
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/free-mode';
 
 // Loading bootstrap with optional features
 initBootstrap({
@@ -32,7 +33,7 @@ footerAccordionUx();
 
 // Half carousel
 const slidesImages = document.getElementsByClassName('postcard-img')
-const halfCarousel = new Swiper('.postcard-carousel', {
+const postcardCarousel = new Swiper('.postcard-carousel', {
 	slidesPerView: "auto",
 	spaceBetween: 16,
     speed: 750,
@@ -40,7 +41,7 @@ const halfCarousel = new Swiper('.postcard-carousel', {
 		el: ".swiper-pagination",
 		clickable: true,
 	},
-	modules: [ Autoplay, Navigation, Pagination ],
+	modules: [ Pagination ],
     on: {
 		activeIndexChange: function() {
 			featuresBgOpacity(slidesImages, this.activeIndex)
@@ -52,5 +53,75 @@ const halfCarousel = new Swiper('.postcard-carousel', {
 const highlightsCarousel = new Swiper('#highlights-carousel', {
 	slidesPerView: "auto",
 	spaceBetween: 16,
-	modules: [ Autoplay, Navigation, Pagination ],
+	modules: [ Navigation, Pagination ],
+});
+
+// Zones Ship carousel
+const zonesShipCarousel = new Swiper('.zones-carousel', {
+	slidesPerView: "auto",
+	spaceBetween: 0,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	modules: [ Pagination ],
+});
+
+// Deck plan Ship carousel
+const allLegendItems = document.getElementsByClassName('legend-item');
+const slide0Active = document.getElementsByClassName('slide-0-active');
+const slide1Active = document.getElementsByClassName('slide-1-active');
+const slide2Active = document.getElementsByClassName('slide-2-active');
+
+const activeLegend = (index) => {
+	for (let i = 0; i < allLegendItems.length; i ++) {
+		allLegendItems[i].classList.remove('active');
+	}
+
+	let activeChuck = slide0Active;
+
+	if (index === 1) {
+		activeChuck = slide1Active;
+	} else if (index === 2) {
+		activeChuck = slide2Active;
+	} else {
+		activeChuck = slide0Active;
+	}
+
+	for (let i = 0; i < activeChuck.length; i ++) {
+		activeChuck[i].classList.add('active');
+	}
+}
+
+const deckCarousel = new Swiper('.deck-carousel', {
+	slidesPerView: "auto",
+	spaceBetween: 0,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	on: {
+		activeIndexChange: function() {
+			activeLegend(this.activeIndex)
+		}
+	},
+	modules: [ Autoplay, Pagination ],
+});
+
+const legendDesktop = document.getElementById('legend-desktop');
+const legendMobile = document.getElementById('legend-mobile');
+const template = document.getElementById('legend-template').content;
+
+legendDesktop.appendChild(template.cloneNode(true));
+legendMobile.appendChild(template.cloneNode(true));
+
+// History carousel
+const historyCarousel = new Swiper('.history-carousel', {
+	slidesPerView: 'auto',
+	spaceBetween: 0,
+	navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+	modules: [ Navigation ],
 });
